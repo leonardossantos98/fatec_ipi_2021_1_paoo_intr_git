5eb07bc83e99b1fc832613b963dde0099b6c1608
Leonardo Santos <leonardosantosneo037@gmail.com>
2021-02-14 14:08:58 -0300

    Exercicio finalizado. Ja pede o nome do usuario e exibe. Pronto para entregar

--- EntregaGit.java
+++ EntregaGit.java
@@ -1,5 +1,10 @@
+import javax.swing.JOptionPane;
 public class EntregaGit{
     public static public static void main(String[] args) {
+        String nome = JOPtionPane.showInputDialog("Qual o seu nome?");
+        JOPtionPane.showMessageDialog(null, "Bem Vindo " + nome);
         System.out.println("Entregando tarefas com o Git na linha de comando");
+        //teste
+        //outro teste
     }
 }
